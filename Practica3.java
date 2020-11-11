63b40ffb1469b93d1a8fc77cdd5c366a324a3ff5
Delmiro <delmirodb@gmail.com>
2020-11-11 19:20:06 +0100

    Cuarto cambio

--- Practica3.java
+++ Practica3.java
@@ -5,6 +5,7 @@ public class Practica3 {
         Scanner sc = new Scanner(System.in);
 
 	//Segundo cambio
+	//Cuarto cambio 
 
         System.out.println("¿Sobre qué tipo de estructura quieres resolver ejercicios?");
         System.out.println("1. if");
